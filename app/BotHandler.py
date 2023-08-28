9156d05fc4c567cd8f6dd5952422db2d126b43d9
pascal <pascal@marascientific.org>
2023-08-28 23:14:18 +0300

    bot handler to functional

--- app/BotHandler.py
+++ app/BotHandler.py
@@ -12,54 +12,34 @@ from app.Users import Users
 from dotenv import load_dotenv
 from aiogram import types
 
-class BotHandler():
-    def __init__(self):
-        load_dotenv()
-        self.TOKEN = getenv('TOKEN')
-        self.bot = Bot(self.TOKEN, parse_mode=ParseMode.HTML)
-        self.dp = Dispatcher(self.bot)
+load_dotenv()
+TOKEN = getenv('TOKEN')
+bot = Bot(TOKEN, parse_mode=ParseMode.HTML)
+dp = Dispatcher(bot)
 
-        # Register the handlers
-        self.register_handlers()
+@dp.message_handler(content_types=types.ContentTypes.TEXT)
+async def start_handler(message: types.Message):
+    if message.text.lower() == "start":
+        await message.answer(f"Hello, {hbold(message.from_user.first_name)}!")
+        await message.answer(f"Welcome to Eatsir 😄")    
+        await message.answer(f"You know what to do... Go ahead!")  
 
+     
+@dp.message_handler(content_types=types.ContentTypes.TEXT)
+async def login_handler(message: types.Message):
+        print('hello')
+        password =  message.text
+        username = message.from_user.first_name
+    
+        await message.answer(f"Hold on...")
+        user = Users.get_user(username, password)
+        if user is not None:
+            # Display user info
+            await message.answer("Welcome back!")
+        else:
+            # Ask if they want to get started
+            await message.answer("Looks like you don't know what to do!")
+            await message.answer("Input your passocode or create one 👇", reply_markup=types.ReplyKeyboardMarkup(resize_keyboard=True).add("Create"))
 
-    def register_handlers(self):
-        @self.dp.message_handler(CommandStart())
-        async def command_start_handler(message: Message) -> None:
-            await message.answer(f"Hello, {hbold(message.from_user.first_name)}!")
-            await message.answer(f"Welcome to Eatsir 😄", reply_markup=types.ReplyKeyboardMarkup(
-                keyboard=[
-                    [types.KeyboardButton(text="Login")],
-                    [types.KeyboardButton(text="Register")]
-                ],
-                resize_keyboard=True
-            ))
-        
-        @self.dp.message_handler(content_types=types.ContentTypes.TEXT)
-        async def login(message: types.Message):
-            if message.text == "Login":
-                # Perform login logic
-                username = message.from_user.first_name
-                await message.answer("Please enter your password:")
-                password = message.text
-
-                print(password)
-                await message.answer("Hold on...")  # Show a message to indicate that the code is running
-                user = Users.get_user(username, password)
-                if user is not None:
-                    # Display user info
-                    await message.answer("Welcome back!")
-                else:
-                    # Ask if they want to get started
-                    await message.answer("You are not yet in our system. Do you want to get started?")
-            elif message.text == "Register":
-                # Perform registration logic
-                # Call the registration function here
-                await self.register(message)
-
-
-    async def main(self) -> None:
-        await self.dp.start_polling()
-
-
-
+async def main() -> None:
+    await dp.start_polling()
--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import asyncio
 import logging
 import sys
-from app.BotHandler import BotHandler
+from app.BotHandler import main
 
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
     
-    asyncio.run(BotHandler().main())
+    asyncio.run(main())
